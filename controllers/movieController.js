6652f0188878ae94fb022e71741c8be7ea0d2e22
DemiusLs <dloss320@gmail.com>
2025-06-26 18:19:30 +0200

    feat : index and show query done

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,7 +2,7 @@ import connection from "../db.js";
 
 const index = (req, res) => {
 
-    const sql = 'SELECT * FROM movies';
+    const sql = 'SELECT movies.* , ROUND(AVG(reviews.vote) , 2) AS avg_vote FROM movies LEFT JOIN  reviews ON movies.id = reviews.movie_id GROUP BY movies.id';
 
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
@@ -14,10 +14,31 @@ const index = (req, res) => {
 const show = (req, res) => {
 
     const id = req.params.id
+    const sqlMovies = 'SELECT * FROM movies WHERE id= ?';
+    const sqlReview = 'SELECT * FROM reviews WHERE id= ?';
 
-    res.json(
-        `risposta da show con id ${id}`
-    )
+    connection.query(sqlMovies, [id], (err, moviesResults) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (moviesResults.length === 0) {
+            return res.status(404).json({ error: 'Movie not found' });
+        } else {
+            connection.query(sqlReview, [id], (err, reviewsResults) => {
+
+                console.log(reviewsResults)
+                res.json({
+                    data: {
+                        ...moviesResults[0],
+                        reviews: reviewsResults,
+                    },
+                });
+
+            })
+
+
+        }
+
+
+    });
 
 }
 
